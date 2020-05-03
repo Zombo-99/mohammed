b45daf912ef98eb6b5b6ea977fa4b9ca0a30c274
zombo-31 <m.bouahdjeb@esi-sba.dz>
2020-05-03 01:19:43 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # mohammed
+# mohammed
---
+++ user/me/route.js
@@ -0,0 +1,16 @@
+/**
+ * @method - GET
+ * @description - Get LoggedIn User
+ * @param - /user/me
+ */
+
+
+router.get("/me", auth, async (req, res) => {
+    try {
+      // request.user is getting fetched from Middleware after token authentication
+      const user = await User.findById(req.user.id);
+      res.json(user);
+    } catch (e) {
+      res.send({ message: "Error in Fetching user" });
+    }
+  });
